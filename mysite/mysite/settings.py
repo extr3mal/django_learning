a32151f4d0fe846b15b3afe6fa895074e37e9ee8
Valentin Rozlovenco <valentin.rozlovenco@sweetstackdigital.com>
2020-03-20 18:36:10 +0200

    updated settings file

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -51,11 +51,12 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'mysite.urls'
+SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'newapp/templates')],
+        'DIRS': [os.path.join(SETTINGS_PATH, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
