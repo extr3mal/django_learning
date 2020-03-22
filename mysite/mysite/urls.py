52df0d1e609a1a6fc6b4bf1d90f6223bff1bceda
Valentin Rozlovenco <valentin.rozlovenco@sweetstackdigital.com>
2020-03-22 08:25:52 +0200

    Added new files

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -21,5 +21,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('newapp.urls')),
     path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
